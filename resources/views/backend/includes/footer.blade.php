a38450efa90e281a116ee209827e3bd93b7044ae
zakria tunio <muhammadzakriatunio@gmail.com>
2026-08-25 10:32:53 +0500

    Revamp footer section with new design and text
    Updated footer layout and content with new branding.

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -1,16 +1,22 @@
 
 {{-- Footer Section Start --}}
 <footer class="footer pr-hide sticky {{ getCustomizationSetting('footer_style') }}">
-  <div class="footer-body">
-      <div class="left-panel">
-          <a href="{{ route('backend.home') }}">{{setting('app_name')}}</a>.
-          {{ setting('copyright_text') }}
-      </div>
-      <div class="center-panel">
-          {!! setting('footer_text') !!}
-      </div>
-      <div class="end-panel">
-        {!! setting('ui_text') !!}
-      </div>
-  </div>
+<div class="footer-body">
+    <div class="left-panel">
+        © Cosmo Doctors {{ now()->year }}. All Rights Reserved
+    </div>
+
+    <div class="center-panel"></div>
+
+    <div class="end-panel">
+        Powered By
+        <a
+            href="https://www.digivisor.co.uk/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Digivisor
+        </a>
+    </div>
+</div>
 </footer>
