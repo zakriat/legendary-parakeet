03490dc7cd75042bd6f3a5ebe1c0bd4ee3aa34f0
zakria tunio <muhammadzakriatunio@gmail.com>
2026-08-30 17:44:00 +0500

    Refactor action column for nurse management
    Refactor change password button to use modal and update delete button confirmation message.

--- Modules/Clinic/Resources/views/backend/nurse/action_column.blade.php
+++ Modules/Clinic/Resources/views/backend/nurse/action_column.blade.php
@@ -7,11 +7,38 @@ $isDemo = env('IS_DEMO', false);
     <button type="button" class="btn text-primary p-0 fs-5" data-crud-id="{{$data->id}}" title="{{ __('messages.edit') }} {{ __('nurse.singular_title') }}" data-bs-toggle="tooltip"> <i class="fa-solid fa-pen-clip"></i></button>
     @endhasPermission
     
-    <button type="button" class="btn text-primary p-0 fs-5" data-assign-module="nurse" data-assign-target="#nurse-change-password" data-assign-event="nurse_change_password" data-nurse-id="{{$data->id}}" title="{{ __('messages.change_password') }}" data-bs-toggle="tooltip"> <i class="fa-solid fa-key"></i></button>
-    
+<button
+    type="button"
+    class="btn text-primary p-0 fs-5"
+    data-bs-toggle="tooltip"
+    title="{{ __('messages.change_password') }}"
+    onclick="openNursePasswordModal({{ $data->id }})">
+    <i class="fa-solid fa-key"></i>
+</button>
+
+@once
+    @push('after-scripts')
+        <script>
+            function openNursePasswordModal(nurseId) {
+                const modalElement = document.getElementById('nurse-change-password');
+
+                if (!modalElement) {
+                    console.error('Nurse password modal was not found.');
+                    return;
+                }
+
+                document.dispatchEvent(new CustomEvent('nurse_change_password', {
+                    detail: { nurse_id: nurseId }
+                }));
+
+                bootstrap.Modal.getOrCreateInstance(modalElement).show();
+            }
+        </script>
+    @endpush
+@endonce    
     @hasPermission('delete_clinic_nurse_list')
     @if(!$isDemo)
-    <a href="{{route("backend.$module_name.destroy", $data->id)}}" id="delete-{{$module_name}}-{{$data->id}}" class="btn text-primary p-0 fs-5" data-type="ajax" data-method="DELETE" data-token="{{csrf_token()}}" data-bs-toggle="tooltip" title="{{__('messages.delete')}}" data-confirm-yes="{{__('messages.action_warning_message')}}">
+    <a href="{{route("backend.$module_name.destroy", $data->id)}}" id="delete-{{$module_name}}-{{$data->id}}" class="btn text-primary p-0 fs-5" data-type="ajax" data-method="DELETE" data-token="{{csrf_token()}}" data-bs-toggle="tooltip" title="{{__('messages.delete')}}" data-confirm="{{ __('messages.action_warning_message') }}">
         <i class="fa-solid fa-trash"></i>
     </a>
     @endif
